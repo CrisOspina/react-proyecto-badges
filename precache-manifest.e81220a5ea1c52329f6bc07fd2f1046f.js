a571cc5ef3efb1c17f2717ff1167234bf8c35c89
Cristian Ospina <crisospina523@gmail.com>
2019-05-25 17:07:59 -0500

    Updates

---
+++ precache-manifest.e81220a5ea1c52329f6bc07fd2f1046f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af478980fb30d8394a2e3da5c54a71c",
+    "url": "/react-proyecto-badges/index.html"
+  },
+  {
+    "revision": "aaa80be9208e6947c3d6",
+    "url": "/react-proyecto-badges/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "91c016004d2340b67826",
+    "url": "/react-proyecto-badges/static/css/main.2c820164.chunk.css"
+  },
+  {
+    "revision": "aaa80be9208e6947c3d6",
+    "url": "/react-proyecto-badges/static/js/2.6db111bf.chunk.js"
+  },
+  {
+    "revision": "91c016004d2340b67826",
+    "url": "/react-proyecto-badges/static/js/main.5b78d1d4.chunk.js"
+  },
+  {
+    "revision": "d7c389e8bff5e4945e6f",
+    "url": "/react-proyecto-badges/static/js/runtime~main.5f4f7389.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-proyecto-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-proyecto-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-proyecto-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-proyecto-badges/static/media/platzi-conf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-proyecto-badges/static/media/stars.1d17360a.svg"
+  }
+]);
