1c807493f309b49495204190384316d99f57aa8a
Cristian Ospina <crisospina523@gmail.com>
2019-05-25 16:21:09 -0500

    Actualización package.json

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:3001';
+// const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://crisospina.github.io';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
